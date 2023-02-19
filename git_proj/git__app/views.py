97991b0165a00a89816b8c1dc1046d1e8118f85b
msoleyev <msoleyev@gmail.com>
2023-02-19 17:42:15 +0300

    add view

--- git_proj/git__app/views.py
+++ git_proj/git__app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
